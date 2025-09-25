5c931bd066ff8036b39a777f493a0532a81c9434
Muhammad Hafidz Arbanov <apitarbanov04@gmail.com>
2025-09-25 12:18:04 +0700

    change logout button color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,7 +63,7 @@ export default function Navbar() {
                   localStorage.removeItem("token");
                   setIsLoggedIn(false);
                 }}
-                className="px-4 py-2 text-sm font-medium rounded-lg bg-red-500 text-white hover:bg-red-600"
+                className="px-4 py-2 text-sm font-medium rounded-lg bg-gray-500 text-white hover:bg-gray-600"
               >
                 Logout
               </button>
